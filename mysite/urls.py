48ff55cbd6e692be04e2a44ad9bf0dad1f0e5e88
Nirali Pachchigar <nnpachchigar1@buffs.wtamu.edu>
2021-12-02 15:11:09 -0600

    update main urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,7 +31,6 @@ urlpatterns = i18n_patterns (
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('images/', include('images.urls', namespace='images')),
-    path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
           name='django.contrib.sitemaps.views.sitemap'),
     path(_('cart/'), include('cart.urls', namespace='cart')),
@@ -39,10 +38,13 @@ urlpatterns = i18n_patterns (
     path(_('payment/'), include('payment.urls', namespace='payment')),
     path(_('coupons/'), include('coupons.urls', namespace='coupons')),
     path('rosetta/', include('rosetta.urls')),  
-    path('', include('shop.urls', namespace='shop')),      
+    path('shop/', include('shop.urls', namespace='shop')),    
+    path('', include('blog.urls', namespace='blog')),  
 )
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, 
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, 
+                          document_root=settings.STATIC_ROOT)                      
           
