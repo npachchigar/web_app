8d8b2dff2171c717a541107b53827a848077f5f8
Nirali Pachchigar <nnpachchigar1@buffs.wtamu.edu>
2021-12-05 03:02:07 -0600

    change the settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,12 +96,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogdb',
-        'USER': 'blog_a',
-        'PASSWORD': 'HQ_5qY+ds>*d#2!A',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
